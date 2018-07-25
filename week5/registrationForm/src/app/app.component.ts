60f4f3a2218ff1465a424d29417a9454faf8a17b
Ben <x329457973@gmail.com>
2018-07-25 10:35:18 -0700

    regit form

--- week5/registrationForm/src/app/app.component.ts
+++ week5/registrationForm/src/app/app.component.ts
@@ -9,12 +9,14 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  template ="";
   user = new User();
   users =[];
   onSubmit(event:Event, form: NgForm){
     event.preventDefault();
     this.users.push(this.user);
     this.user = new User();
+    this.template ="Thank for registering with us.",this.users," We just sent you a confirmation email at, and will send all mail to.   Have a wonderful day!"
     form.reset();
   }
 }
